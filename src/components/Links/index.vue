107603429773dcd53abe0be935b525e67e2e7dc1
Luozongwei <99460024+Luozongwei@users.noreply.github.com>
2023-05-28 20:18:03 +0800

    update 1

--- src/components/Links/index.vue
+++ src/components/Links/index.vue
@@ -46,12 +46,12 @@ let linksData = [
   {
     icon: Blog,
     name: "博客",
-    link: "https://blog.imsyy.top/",
+    link: "https://luozongwei.github.io/",
   },
   {
     icon: Cloud,
     name: "网盘",
-    link: "https://pan.imsyy.top/",
+    link: "https://pan.baidu.com/disk/main#/index?category=all",
   },
   {
     icon: CompactDisc,
